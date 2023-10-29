ee897a16a22241da2c8c79a3a629cc58b84f0680
Nourhan Mahmoud <134957428+NourhanMahmoud-0@users.noreply.github.com>
2023-10-30 00:22:22 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+let img = document.querySelector('#img');
+let contaner = document.querySelector('.contaner');
+
+function watches(wat){
+    img.src = wat;
+}
+function colors(color){
+    contaner.style.background = color;
+}
